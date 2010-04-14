aa68fe676b3d135e6218ba2e9b4225d6f6ae0e88
Liam M. Healy <lnp@healy.washington.dc.us>
2010-04-14 16:08:17 -0400

    Correct return values from jacobian-elliptic-functions; add tests
    The GSL function gsl_sf_elljac_e writes sn, cn, dn values to doubles
    for which the pointer has been passed in, as shown in
    /usr/include/gsl/gsl_sf_elljac.h, and not to sf-result as one might be
    led to conclude from the "_e" at the end of the name.  This is now
    fixed in GSLL, so that the tests, which sometimes passed and sometimes
    didn't depending on what was run before it, now should always work.
    The single non-error test been replaced by the 35 GSL tests in
    gsl/specfunc/test_sf.c, and they all pass in SBCL Linux AMD64.

--- special-functions/elliptic-functions.lisp
+++ special-functions/elliptic-functions.lisp
@@ -1,6 +1,6 @@
 ;; Jacobian elliptic functions
 ;; Liam Healy, Mon Mar 20 2006 - 22:21
-;; Time-stamp: <2009-12-27 10:10:05EST elliptic-functions.lisp>
+;; Time-stamp: <2010-04-14 13:06:10EDT elliptic-functions.lisp>
 ;;
 ;; Copyright 2006, 2007, 2008, 2009 Liam M. Healy
 ;; Distributed under the terms of the GNU General Public License
@@ -20,25 +20,59 @@
 
 (in-package :gsl)
 
+;; /usr/include/gsl/gsl_sf_elljac.h
+
 (defmfun jacobian-elliptic-functions (u m)
   "gsl_sf_elljac_e"
-  ((u :double) (m :double) (sn sf-result) (cn sf-result) (dn sf-result))
-  :return ((val sn) (val cn) (val dn) (err sn) (err cn) (err dn))
+  ((u :double) (m :double)
+   (sn (:pointer :double)) (cn (:pointer :double)) (dn (:pointer :double)))
   :documentation			; FDL
   "The Jacobian elliptic functions sn(u|m),
   cn(u|m), dn(u|m) computed by descending Landen transformations.")
 
-;;; > (jacobian-elliptic-functions 0.61802d0 0.5d0)
-;;; 0.564575752943391
-;;; 0.8253812568676386
-;;; 0.916857191493965
-;;; > (jacobian-elliptic-functions 0.2d0 0.81d0)
-;;; 0.19762082367187697
-;;; 0.9802785369736752
-;;; 0.9840560289645665
-;;; > (jacobian-elliptic-functions 0.61802d0 1.5d0)
-;;; ;;;error
+(defvar *elljac-K* (/ pi (* 2.0d0 0.9741726903999478375938128316d0)))
+(defvar *elljac-A* (/ (sqrt (1+ (sqrt 0.9d0)))))
+(defvar *elljac-B* (/ (expt 0.9d0 1/4) (sqrt (1+ (sqrt 0.9d0)))))
+(defvar *elljac-C* (expt 0.9d0 1/4))
+(defvar *elljac-C2* (sqrt 0.9d0))
 
-(save-test elliptic-functions
-	   (jacobian-elliptic-functions 0.2d0 0.81d0)
-	   (jacobian-elliptic-functions 0.61802d0 1.5d0))
+(save-test
+ elliptic-functions
+ ;; Tests from gsl/specfunc/test_sf.c
+ (jacobian-elliptic-functions 0.5d0 0.5d0)
+ (jacobian-elliptic-functions 1.0d0 0.3d0)
+ (jacobian-elliptic-functions 1.0d0 0.6d0)
+ (jacobian-elliptic-functions 2.0d0 0.999999d0)
+ (jacobian-elliptic-functions 1.69695970624443d0 0.270378013104138d0)
+ (jacobian-elliptic-functions 0.0d0 0.1d0)
+ (jacobian-elliptic-functions -1.0d-10 0.1d0)
+ (jacobian-elliptic-functions 1.0d-10 0.1d0)
+ (jacobian-elliptic-functions 1.0d-30 0.1d0)
+ (jacobian-elliptic-functions (- (/ *elljac-K* 2.0d0) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (/ *elljac-K* 2.0d0) 0.1d0)
+ (jacobian-elliptic-functions (+ (/ *elljac-K* 2.0d0) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (- *elljac-K* 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions *elljac-K* 0.1d0)
+ (jacobian-elliptic-functions (+ *elljac-K* 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 3/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (- (* 2 *elljac-K*) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (+ (* 2 *elljac-K*) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 5/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (- (* 3 *elljac-K*) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 3 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (+ (* 3 *elljac-K*) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 7/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (- (* 4 *elljac-K*) 1.0d-10) 0.1d0)
+ (jacobian-elliptic-functions (* 4 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* 9/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -1/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -1 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -3/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -5/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -3 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -7/2 *elljac-K*) 0.1d0)
+ (jacobian-elliptic-functions (* -4 *elljac-K*) 0.1d0)
+ ;; Signal an error
+ (jacobian-elliptic-functions 0.61802d0 1.5d0))
