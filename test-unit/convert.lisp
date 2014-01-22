75b6667f9c44678db61d307543264581ff924245
Liam M. Healy <lhealy@common-lisp.net>
2014-01-21 22:50:14 -0500

    Restart semi-automatic translation of GSL tests

--- test-unit/convert.lisp
+++ test-unit/convert.lisp
@@ -1,6 +1,22 @@
 ;; Convert the GSL tests
 ;; Liam Healy 2010-05-22 13:03:53EDT convert.lisp
-;; Time-stamp: <2010-07-21 13:27:06EDT convert.lisp>
+;; Time-stamp: <2014-01-21 16:16:59EST convert.lisp>
+
+;; Copyright 2010, 2014 Liam M. Healy
+;; Distributed under the terms of the GNU General Public License
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; This file is not normally loaded; it is only used to convert the
 ;;; GSL tests in C to CL tests.  It requires cl-ppcre, alexandria, and iterate.
